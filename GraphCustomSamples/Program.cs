06c50588170248106fa801a7aaad8ad6154262d6
Christoph Wille <christoph.wille@gmail.com>
2025-02-07 14:48:53 +0100

    Custom SDK, iterate with OdataNextLink as there is no PageIterator

--- GraphCustomSamples/Program.cs
+++ GraphCustomSamples/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Kiota.Authentication.Azure;
 using Microsoft.Kiota.Http.HttpClientLibrary;
 using NoBackingStore.Graph;
+using NoBackingStore.Graph.Models;
 
 var builder = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -21,6 +22,33 @@ var tokenProvider = new AzureIdentityAuthenticationProvider(clientSecretCredenti
 var requestAdapter = new HttpClientRequestAdapter(tokenProvider);
 
 var client = new UsersClient(requestAdapter);
-var result = await client.Users.GetAsync();
+
+// There is no PageIterator in the generated code
+
+List<User> users = new();
+var resultPageOne = await client.Users.GetAsync(requestConfiguration =>
+    {
+        requestConfiguration.QueryParameters.Top = 10;
+    });
+
+if (resultPageOne == null)
+{
+    Console.WriteLine("No users found. Program ended");
+    return;
+}
+
+users.AddRange(resultPageOne.Value);
+var pageRequest = resultPageOne.OdataNextLink;
+int pageRequests = 0;
+
+while (pageRequest != null)
+{
+    var nextPage = await client.Users.WithUrl(pageRequest).GetAsync();
+    users.AddRange(nextPage.Value);
+    pageRequest = nextPage.OdataNextLink;
+    pageRequests++;
+}
+
+Console.WriteLine($"Done. {users.Count} entities and {pageRequests} page requests issued");
 
 Console.WriteLine("Program ended");
