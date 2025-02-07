5ffe33a9b1cf7bebad7de3e7c9025f8c9a6dd277
Christoph Wille <christoph.wille@gmail.com>
2025-02-07 14:14:11 +0100

    Version with ToGetRequestInformation and RequestAdapter.SendAsync

--- GraphSdkSamples/Program.cs
+++ GraphSdkSamples/Program.cs
@@ -3,6 +3,8 @@ using GraphVariations.Common;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Graph;
 using Microsoft.Graph.Models;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Serialization;
 
 var builder = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -34,26 +36,38 @@ const int pageSize = 10;
 var resultSingleCall = await graphClient.Users.GetAsync();
 Console.WriteLine($"Done simple. {resultSingleCall.Value.Count} entities");
 
-// #2 - use the standard iterator pattern
+// #2 - use the standard iterator pattern from https://learn.microsoft.com/en-us/graph/sdks/paging?tabs=csharp
 var resultIterator = await PageViaIteratorAsync<User, UserCollectionResponse>(() => graphClient.Users.GetAsync(requestConfiguration =>
 {
     requestConfiguration.QueryParameters.Top = pageSize;
 }));
 Console.WriteLine($"Done iterator. {resultIterator.Entities.Count} entities and {resultIterator.PageRequests} page requests issued");
 
-// #3 - OdataNextLink
-var resultOdataLink = await PageViaOdataLinkAsync<User, UserCollectionResponse>(
-    () => graphClient.Users.GetAsync(requestConfiguration =>
+// #3 - OdataNextLink. Generic variant of https://learn.microsoft.com/en-us/graph/sdks/paging?tabs=csharp#manually-requesting-subsequent-pages
+//var resultOdataLink = await PageViaOdataLinkAsync<User, UserCollectionResponse>(
+//    () => graphClient.Users.GetAsync(requestConfiguration =>
+//            {
+//                requestConfiguration.QueryParameters.Top = pageSize;
+//            }),
+//    nextLink => graphClient.Users.WithUrl(nextLink).GetAsync());
+//Console.WriteLine($"Done OdataNextLink. {resultOdataLink.Entities.Count} entities and {resultOdataLink.PageRequests} page requests issued");
+
+// #4 -RequestInformation
+//var requestInfo = graphClient.Users.ToGetRequestInformation(requestConfiguration =>
+//            {
+//                requestConfiguration.QueryParameters.Top = pageSize;
+//            });
+//var user = await graphClient.RequestAdapter.SendAsync(requestInfo, UserCollectionResponse.CreateFromDiscriminatorValue);
+var resultRequestInfo = await PageViaRequestInformationAsync<User, UserCollectionResponse>(
+            graphClient.Users.ToGetRequestInformation(requestConfiguration =>
             {
                 requestConfiguration.QueryParameters.Top = pageSize;
             }),
-    nextLink => graphClient.Users.WithUrl(nextLink).GetAsync());
-Console.WriteLine($"Done OdataNextLink. {resultOdataLink.Entities.Count} entities and {resultOdataLink.PageRequests} page requests issued");
+            UserCollectionResponse.CreateFromDiscriminatorValue);
+Console.WriteLine($"Done RequestInformation. {resultRequestInfo.Entities.Count} entities and {resultRequestInfo.PageRequests} page requests issued");
 
+Console.WriteLine("Program ended");
 
-
-
-// https://learn.microsoft.com/en-us/graph/sdks/paging?tabs=csharp
 async Task<PagedRequestResponse<TEntity>> PageViaIteratorAsync<TEntity, TCollectionPage>(Func<Task<TCollectionPage?>> getFirstPage)
     where TCollectionPage : Microsoft.Graph.Models.BaseCollectionPaginationCountResponse, new()
 {
@@ -84,7 +98,6 @@ async Task<PagedRequestResponse<TEntity>> PageViaIteratorAsync<TEntity, TCollect
     return new(retval, pageRequests);
 }
 
-// Generic variant of https://learn.microsoft.com/en-us/graph/sdks/paging?tabs=csharp#manually-requesting-subsequent-pages
 async Task<PagedRequestResponse<TEntity>> PageViaOdataLinkAsync<TEntity, TCollectionPage>(
         Func<Task<TCollectionPage?>> getFirstPage,
         Func<string, Task<TCollectionPage?>> getNextPage)
@@ -105,6 +118,35 @@ async Task<PagedRequestResponse<TEntity>> PageViaOdataLinkAsync<TEntity, TCollec
     while (pageRequest != null)
     {
         var nextPage = await getNextPage(pageRequest);
+        retval.AddRange(nextPage.BackingStore?.Get<List<TEntity>?>("value"));
+        pageRequest = nextPage.OdataNextLink;
+        pageRequests++;
+    }
+
+    return new(retval, pageRequests);
+}
+
+async Task<PagedRequestResponse<TEntity>> PageViaRequestInformationAsync<TEntity, TCollectionPage>(
+        RequestInformation requestInfo, ParsableFactory<TCollectionPage> factory)
+    where TCollectionPage : Microsoft.Graph.Models.BaseCollectionPaginationCountResponse, new()
+    where TEntity : IParsable
+{
+    List<TEntity> retval = new();
+
+    TCollectionPage? firstPage = await graphClient.RequestAdapter.SendAsync(requestInfo, factory);
+    if (null == firstPage) return new(retval);
+
+    // BaseCollectionPaginationCountResponse does not have the .Value property (who designed Kiota codegen???)
+    retval.AddRange(firstPage.BackingStore?.Get<List<TEntity>?>("value"));
+
+    // Get subsequent pages
+    var pageRequest = firstPage.OdataNextLink;
+    int pageRequests = 0;
+
+    while (pageRequest != null)
+    {
+        requestInfo.URI = new Uri(pageRequest);
+        var nextPage = await graphClient.RequestAdapter.SendAsync(requestInfo, factory);
         retval.AddRange(nextPage.BackingStore?.Get<List<TEntity>?>("value"));
         pageRequest = nextPage.OdataNextLink;
         pageRequests++;
